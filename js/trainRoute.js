ea5cad91f7d916c5636afb15058170f7c00776cb
DoctorTone <drtonesoftware@gmail.com>
2017-08-30 12:17:16 +0100

    Reduce stops, change stop names

--- js/trainRoute.js
+++ js/trainRoute.js
@@ -3,32 +3,22 @@
  */
 //Simulates reading data in from file
 
-var trainRoute = {
+let trainRoute = {
     
     id: "P0347220151021",
     startTime: 0,
     routeData: [
-        {stationName: "STOP1", time: 0, delay: 1},
-        {stationName: "STOP2", time: 0, delay: 1},
-        {stationName: "STOP3", time: 2, delay: 1.5},
-        {stationName: "STOP4", time: 3, delay: 1},
-        {stationName: "STOP5", time: 3, delay: 1},
-        {stationName: "STOP6", time: 4, delay: 0},
-        {stationName: "STOP7", time: 7, delay: 0},
-        {stationName: "STOP8", time: 8, delay: -1},
-        {stationName: "STOP9", time: 8, delay: -0.5},
-        {stationName: "STOP10", time: 9, delay: 0},
-        {stationName: "STOP11", time: 10, delay: 0},
-        {stationName: "STOP12", time: 11, delay: 0},
-        {stationName: "STOP13", time: 12, delay: -0.5},
-        {stationName: "STOP14", time: 15, delay: 0},
-        {stationName: "STOP15", time: 16, delay: 0.5},
-        {stationName: "STOP16", time: 17, delay: 0},
-        {stationName: "STOP17", time: 17, delay: -1},
-        {stationName: "STOP18", time: 18, delay: -1},
-        {stationName: "STOP19", time: 20, delay: -2},
-        {stationName: "STOP20", time: 20, delay: -2},
-        {stationName: "STOP21", time: 24, delay: 0}
+        {stationName: "Station 1", time: 0, delay: 1},
+        {stationName: "Station 2", time: 2, delay: 1.5},
+        {stationName: "Station 3", time: 3, delay: 1},
+        {stationName: "Station 4", time: 7, delay: 0},
+        {stationName: "Station 5", time: 8, delay: -0.5},
+        {stationName: "Station 6", time: 10, delay: 0},
+        {stationName: "Station 7", time: 12, delay: -0.5},
+        {stationName: "Station 8", time: 16, delay: 0.5},
+        {stationName: "Station 9", time: 17, delay: -1},
+        {stationName: "Station 10", time: 20, delay: -2},
+        {stationName: "Station 11", time: 24, delay: 0}
     ]
 };
 
