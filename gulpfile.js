994ae2888a84ff9fbbae32da753bb86eaa6db2e4
DoctorTone <drtonesoftware@gmail.com>
2017-10-26 17:59:51 +0100

    Added gulp packages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var changed = require("gulp-changed");
 
 gulp.task("build", ["compile", "min-copy"], function() {
 
 });
 
+var DEST = "./temp/js";
 gulp.task("compile", function() {
     return gulp.src(["./js/*.js", "!./js/*.min.js"])
+        .pipe(changed(DEST))
         .pipe(babel())
-        .pipe(gulp.dest("./dist/js"));
+        .pipe(gulp.dest(DEST));
 });
 
 gulp.task("min-copy", function() {
-    return gulp.src("./js/*.min.js")
-        .pipe(gulp.dest("./dist/js"))
+    gulp.src("./js/*.min.js")
+        .pipe(gulp.dest("./build/js"));
+    gulp.src("./css/*.min.css")
+        .pipe(gulp.dest("./build/css"));
 });
